664c2e2caba5d2466c0181a9f611e1631da80494
Home Desktop <kkumong9@gmail.com>
2019-09-04 19:24:08 +0900

    home test

--- src/Components/App/AppContainer.tsx
+++ src/Components/App/AppContainer.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import { graphql } from "react-apollo";
 import { IS_LOGGED_IN } from "./AppQueries";
 
-const AppContainer = () => <div>Stuff</div>;
+const AppContainer = () => <div>Stufff</div>;
 
 export default graphql(IS_LOGGED_IN)(AppContainer);
